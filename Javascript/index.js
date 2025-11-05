90a49936242dadce99c16fb241053ec60b0c2cb2
samyogadk24 <samyogdbs19@gmail.com>
2025-11-05 15:39:33 -0800

    Fix: remove submodule reference

---
+++ Javascript/index.js
@@ -0,0 +1,39 @@
+// variable declerations 
+
+let name = "sneaky ";
+
+name="sanam"
+console.log(name);
+
+// array
+const fruit =["apple","banana","mango"];
+console.log(fruit);
+
+//objects 
+
+const person = ["Leader","Sneaky","Biraj"];
+console.log(person);
+
+
+
+
+function oddeven (a) {
+ if (a%2==0){
+    console.log("even");
+   } else {
+    console.log("odd");
+   }
+}
+console.log(oddeven(2));
+
+
+function greater(num1, num2) {
+   if (num1>num2) {
+    console.log("Number 1 is greater");
+   } else {
+    console.log("Number 2 is greater");
+
+   }
+}
+
+console.log(greater(2,4));
