dfa1458c9e8356f65d0522fd387caec4f423cbeb
samyogadk24 <samyogdbs19@gmail.com>
2025-11-19 15:12:49 -0800

    commit

--- Javascript/Sumnum.js
+++ Javascript/Sumnum.js
@@ -1,8 +1,10 @@
- 
+ function sumnum(){
     let sum =0;
 
-    for(let i=0; i<=5; i++ ){
-        console.log(sum);
+    for(let i=0; i<5; i++ ){
+      
         sum= sum+i ;
-    
-    }
+    console.log(sum);
+    } 
+} 
+sumnum();
