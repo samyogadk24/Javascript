a6f9d69c89bd88727e883607a9c44e9d519efe26
samyogadk24 <samyogdbs19@gmail.com>
2025-11-19 14:45:10 -0800

    first commit

---
+++ Javascript/Number.js
@@ -0,0 +1,13 @@
+function number(a, b, c) {
+  if (a > b && a > c) {
+    console.log(" a is the greatest");
+  }
+  else if (b > a && b > c) {
+    console.log(" b is the greatest");
+  }
+  else {
+    console.log(" c is the greatest");
+  }
+}
+
+number(5, 2, 1);
