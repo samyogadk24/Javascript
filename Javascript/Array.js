83bb12fbaaae94213b09f7d67c6ac7bb655d503d
samyogadk24 <samyogdbs19@gmail.com>
2025-11-26 14:42:47 -0800

    commit

---
+++ Javascript/Array.js
@@ -0,0 +1,12 @@
+const greater =(a,b,c,)=>{
+      if (a > b && a > c) {
+    console.log(" a is the greatest");
+  }
+  else if (b > a && b > c) {
+    console.log(" b is the greatest");
+  }
+  else {
+    console.log(" c is the greatest");
+  }
+}
+    greater(1,3,4)
--- Javascript/Sumnum.js
+++ Javascript/Sumnum.js
@@ -2,9 +2,11 @@
     let sum =0;
 
     for(let i=0; i<5; i++ ){
-      
+
         sum= sum+i ;
     console.log(sum);
-    } 
+  
+} 
 } 
 sumnum();
+
