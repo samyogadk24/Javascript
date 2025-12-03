000f93cd687f10a5f12a9d651097ea191f863282
samyogadk24 <samyogdbs19@gmail.com>
2025-12-03 14:32:33 -0800

    hw sakiyoo

---
+++ Javascript/Map.js
@@ -0,0 +1,6 @@
+const number = [1,2,3,4,5,6,7,8,9,10];
+const multiply = number.map
+(function (num) {
+    return num * 2;
+});
+console.log(multiply); 
---
+++ Javascript/Object.js
@@ -0,0 +1,12 @@
+const person = {
+    name :"sneakyyyyy..",
+
+    class : 11,
+    
+    age : 17,
+
+    hobbies : ["Aaaaa" ,"Bbbbb","Ccccc"] 
+
+}
+console.log(person.name,person.age,person.class);
+console.log(person.hobbies[0])
